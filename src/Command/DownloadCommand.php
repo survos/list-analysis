633dc16607c263cff42c77d8637d4950ab491322
Tac Tacelosky <tacman@gmail.com>
2018-09-04 20:12:41 -0400

    improve download

--- src/Command/DownloadCommand.php
+++ src/Command/DownloadCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use App\Entity\Archive;
 use Doctrine\ORM\EntityManagerInterface;
 use Goutte\Client;
+use GuzzleHttp\Cookie\CookieJar;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -24,7 +25,7 @@ class DownloadCommand extends ContainerAwareCommand
         $this
             ->setDescription('Add a short description for your command')
             ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addOption('refresh', null, InputOption::VALUE_NONE, 'Refresh Monthly List')
         ;
     }
 
@@ -37,21 +38,28 @@ class DownloadCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
+        $base = 'http://list.rappnet.org/mailman/private/rappnet_list.rappnet.org/';
 
 // makes a real request to an external site
         $client = new Client();
-        $crawler = $client->request('GET', 'http://list.rappnet.org/mailman/private/rappnet_list.rappnet.org/');
+        $crawler = $client->request('GET', $base);
 
 // select the form and fill in some values
         $form = $crawler->selectButton('submit')->form();
         $form['username'] = 'tacman@gmail.com';
         $form['password'] = 'ozwoupid';
 
+
 // submits the given form
         $crawler = $client->submit($form);
 
 
-        // $crawler = $client->request('GET', 'http://list.rappnet.org/mailman/private/rappnet_list.rappnet.org/');
+
+        /*
+
+        $crawler = $client->request('GET', 'http://list.rappnet.org/mailman/private/rappnet_list.rappnet.org/2018-September.txt.gz');
+        $io->writeln(sprintf("%s bytes", $client->getResponse()->getContent())); die();
+        */
 
         // hack, regex the links
         $text = $client->getResponse();
@@ -59,19 +67,65 @@ class DownloadCommand extends ContainerAwareCommand
         // this doesn't seem quite right
         $em = $this->em; // $this->getContainer()->get('doctrine');
         $repo = $em->getRepository(Archive::class);
-        if (preg_match_all('/href="(.*?\.gz)"/', $text, $mm))
+
+
+        if ($input->getOption('refresh'))
         {
-            foreach ($mm[1] as $filename) {
-                if (!$archive = $repo->findOneBy(['filename' => $filename])) {
-                    $archive = (new Archive())
-                        ->setFilename($filename);
-                    $em->persist($archive);
+            if (preg_match_all('/href="(.*?\.gz)"/', $text, $mm))
+            {
+                foreach ($mm[1] as $filename) {
+                    if (!$archive = $repo->findOneBy(['filename' => $filename])) {
+                        $archive = (new Archive())
+                            ->setFilename($filename);
+                        $em->persist($archive);
+                    }
                 }
+            } else {
+                $io->error("No gz files found");
             }
-        } else {
-            $io->error("No gz files found");
+            $em->flush();
         }
-        $em->flush();
+
+
+
+
+        $cookieJar = $client->getCookieJar();
+        $guzzleClient = $client->getClient();
+        $jar = CookieJar::fromArray($cookieJar->all(), 'list.rappnet.org');
+        dump($jar, $cookieJar);
+
+        foreach ($repo->findBy([], ['id' => 'DESC']) as $archive) {
+            $url = $base . $archive->getFilename();
+            $savedFile = "../data/" . $archive->getFilename();
+
+            $crawler = $client->request('GET', $url);
+            file_put_contents($savedFile, $client->getResponse()->getContent());
+            $io->writeln(sprintf("%s: %s bytes", $savedFile, filesize($savedFile)));
+
+
+
+            die();
+
+
+            $response = $guzzleClient->get($url, ['sink' => $savedFile]);
+            $io->writeln($savedFile . " downloaded: " . filesize($savedFile));
+            die("Stopped");
+
+            $response = $client->request('GET', $url, [ 'sink' => $savedFile]);
+            $client->getResponse()->getContent();
+
+            $io->writeln($savedFile . " downloaded: " . filesize($savedFile));
+            die("Stopped");
+
+
+            $response = $guzzleClient->get($url, ['cookies' => $jar, 'sink' => $savedFile]);
+
+            $io->writeln($savedFile . " downloaded: " . filesize($savedFile));
+            die("Stopped");
+        }
+
+
+
 
         /* this is better..
         foreach ($crawler->links() as $link) {
@@ -85,20 +139,7 @@ class DownloadCommand extends ContainerAwareCommand
 
         // dump the results
         // dump($client->getResponse());
-        die("Stopped.");
 
-        $client->request('GET', 'http://list.rappnet.org/mailman/private/rappnet_list.rappnet.org/2018-September.txt.gz');
-
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success('Finished download.');
     }
 }
