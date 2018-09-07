ef3a92e60a190e6919f1dc88e245ea6c8401bbb9
tac <tacman@gmail.com>
2018-09-07 09:25:24 -0400

    rename landing route

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     /**
-     * @Route("/", name="app")
+     * @Route("/landing", name="landing")
      */
     public function index(LandingService $landingService)
     {
